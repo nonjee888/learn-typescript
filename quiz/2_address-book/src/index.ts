c9cecc77f845d343338dfdbd3f328b6c51f02095
nonjee888 <nonjee888@gmail.com>
2023-04-16 22:00:09 +0900

    p263 볼차례

--- quiz/2_address-book/src/index.ts
+++ quiz/2_address-book/src/index.ts
@@ -1,3 +1,9 @@
+enum PhoneType {
+  HOME = "home",
+  OFFICE = "office",
+  STUDIO = "studio",
+}
+
 interface PhoneNumberDictionary {
   [phone: string]: {
     num: number;
@@ -12,9 +18,9 @@ interface Contact {
 
 // api
 // TODO: 아래 함수의 반환 타입을 지정해보세요.
-function fetchContacts(): Contact {
+function fetchContacts(): Promise<Contact[]> {
   // TODO: 아래 변수의 타입을 지정해보세요.
-  const contacts = [
+  const contacts: Contact[] = [
     {
       name: "Tony",
       address: "Malibu",
@@ -57,42 +63,42 @@ function fetchContacts(): Contact {
 // main
 class AddressBook {
   // TODO: 아래 변수의 타입을 지정해보세요.
-  contacts = [];
+  contacts: Contact[] = [];
 
   constructor() {
     this.fetchData();
   }
 
-  fetchData() {
-    fetchContacts().then((response) => {
+  fetchData(): void {
+    fetchContacts().then((response: Contact[]) => {
       this.contacts = response;
     });
   }
 
   /* TODO: 아래 함수들의 파라미터 타입과 반환 타입을 지정해보세요 */
-  findContactByName(name) {
+  findContactByName(name: string): Contact[] {
     return this.contacts.filter((contact) => contact.name === name);
   }
 
-  findContactByAddress(address) {
+  findContactByAddress(address: string): Contact[] {
     return this.contacts.filter((contact) => contact.address === address);
   }
 
-  findContactByPhone(phoneNumber, phoneType: string) {
+  findContactByPhone(phoneNumber: number, phoneType: PhoneType): Contact[] {
     return this.contacts.filter(
       (contact) => contact.phones[phoneType].num === phoneNumber
     );
   }
 
-  addContact(contact) {
+  addContact(contact: Contact): void {
     this.contacts.push(contact);
   }
 
-  displayListByName() {
+  displayListByName(): string[] {
     return this.contacts.map((contact) => contact.name);
   }
 
-  displayListByAddress() {
+  displayListByAddress(): string[] {
     return this.contacts.map((contact) => contact.address);
   }
   /* ------------------------------------------------ */
